590f4c7bffa7a7b5896e9b2871dc3d905503da4a
Lyubomir Dias <lyubomirdias@gmail.com>
2021-03-11 13:53:40 +0000

    created views and controllers for products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
- protect_from_forgery with: :exception
 
   def index
+   # here we find columns in select query and use them as methods on resulting objects
+    @categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # root to: 'pages#home'
   root to: 'categories#index'
+  resources :categories, only: [:index] do
+    resources :products, only: [:index] 
+  end
 end
- 
+  
