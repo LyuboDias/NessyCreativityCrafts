a326961311fbb92cbda481da03ce549e7b9004d2
Lyubomir Dias <lyubomirdias@gmail.com>
2021-03-16 11:33:41 +0000

    testing api

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  stripe_api_key:      ENV['STRIPE_SECRET_KEY']
+  stripe_api_key: ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:stripe_api_key] 
