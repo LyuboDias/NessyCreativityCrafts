82b20e607e52ff4da8840123d01fd0c3f56cd98d
Lyubomir Dias <lyubomirdias@gmail.com>
2021-09-06 20:21:53 +0100

    changed stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
-  # secret_key: ENV['STRIPE_SECRET_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY'],
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
-  :secret_key      => 'STRIPE_SECRET_KEY'
+  
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key] 
